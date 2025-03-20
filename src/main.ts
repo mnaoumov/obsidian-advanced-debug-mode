bbca705a2b49cf96d4280d64ddf03dafd4ec0c83
Michael Naumov <mnaoumov@gmail.com>
2025-03-20 14:00:29 -0600

    Cleanup

--- src/main.ts
+++
@@ -1,5 +0,0 @@
-import './styles/main.scss';
-import { AdvancedDebuggerPlugin } from './AdvancedDebuggerPlugin.ts';
-
-// eslint-disable-next-line import-x/no-default-export
-export default AdvancedDebuggerPlugin;
