5f9423bde2e5a9f4aebce44736d02b1916dd4108
Michael Naumov <mnaoumov@gmail.com>
2025-03-20 13:51:51 -0600

    Template

---
+++ src/main.ts
@@ -0,0 +1,5 @@
+import './styles/main.scss';
+import { AdvancedDebuggerPlugin } from './AdvancedDebuggerPlugin.ts';
+
+// eslint-disable-next-line import-x/no-default-export
+export default AdvancedDebuggerPlugin;
