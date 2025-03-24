b8edea0a366bb7eabd5f9f75bd5ca80c57249d60
Michael Naumov <mnaoumov@gmail.com>
2025-03-23 23:19:35 -0600

    missing undefined check

--- src/LongStackTracesHandler.ts
+++ src/LongStackTracesHandler.ts
@@ -296,5 +296,5 @@ export abstract class LongStackTracesHandler {
 }
 
 function isEventListenerObject(value: unknown): value is EventListenerObject {
-  return (value as Partial<EventListenerObject>).handleEvent !== undefined;
+  return !!(value as Partial<EventListenerObject> | undefined)?.handleEvent;
 }
